8de1dd6939c8f60e566d71b0ecd568b72022f182
Andaj Singh <90814692+andaj004@users.noreply.github.com>
2025-08-17 19:56:23 +0545

    fixing frontend to backend connection

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001',
+  baseURL: process.env.REACT_APP_API_URL || 'http://100.26.42.127:5001',
   headers: { 'Content-Type': 'application/json' },
 });
 
