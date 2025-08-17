57a234dd101e63363326b653b483c9dcb6080297
Andaj Singh <90814692+andaj004@users.noreply.github.com>
2025-08-17 21:48:16 +0545

    Update axiosConfig.jsx with /api

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // Always call relative to current domain
-  baseURL: '',
+  baseURL: '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
